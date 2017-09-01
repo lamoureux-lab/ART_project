9a6b1d3d19be2746c27f74edebe929dd55213312
Justin Whatley <justinwhatley5@gmail.com>
2017-09-01 12:40:43 -0400

    Tabs to spaces for consistency

--- art_dft_v3.1-distr3/using_with_gaussian/scripts/server.py
+++ art_dft_v3.1-distr3/using_with_gaussian/scripts/server.py
@@ -4,7 +4,6 @@ import subprocess
 import time
 
 if __name__=='__main__':
-	for i in range(0,4):
-		time.sleep(3)
-		p = subprocess.Popen("python worker.py %d"%i,shell=True)
-		
+    for i in range(0,4):
+        time.sleep(3)
+        p = subprocess.Popen("python worker.py %d"%i,shell=True)
