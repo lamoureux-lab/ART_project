de04c196dfc573961c30d7a8f1541d71ebe236d9
Bhupinder Singh <bhupindersingh.metneja@mail.concordia.ca>
2018-08-02 12:09:48 -0400

    Also reads the vector log now

--- art_dft_v3.1-distr3/using_with_gaussian/scripts/artgau.py
+++ art_dft_v3.1-distr3/using_with_gaussian/scripts/artgau.py
@@ -1,5 +1,5 @@
 from os.path import dirname, join, relpath, isfile, exists
-from os import makedirs
+from os import makedirs, getcwd, chdir
 import argparse
 import re
 from subprocess import call
@@ -32,6 +32,7 @@ parser.add_argument('-fp', '--force_thresh_perp', help = 'force thresh perp rela
 parser.add_argument('-bm', '--basin_maxiter', help = 'max iterations in basin, e.g. 20', default ='20')
 parser.add_argument('-xyz', '--write_xyz', help = 'write or not xyz files -- true or false', default ='true')
 parser.add_argument('-search', '--search_strat', help = 'search strategy, e.g. 0, 1 or 2', default ='0')
+parser.add_argument('-read', '--read_from', help = 'read from a specific file, e.g. vector20', default ='xxx')
 
 args =                        parser.parse_args()
 
@@ -52,6 +53,8 @@ refconfig =                  'refconfig.dat'
 
 filecounter =                'filecounter'
 
+shared_log =                 '../shared_log/'
+
 files_to_run =                args.gaussian_input
 
 my_dict =                     {}
@@ -60,37 +63,31 @@ for each_file in files_to_run:
     my_dict[each_file] = re.split(r'[.]', each_file)[0]
 
 def create_directory():
-
-    for each_file, directory in my_dict.iteritems():
-
+    for directory in my_dict.itervalues():
         try:
             if not exists(directory):
                 makedirs(directory)
         except OSError:
             print ('Error: Creating directory. ' +  directory)
 
-
 def create_submission_file():
-
     for directory in my_dict.itervalues():        
         with open(submission_script_template) as f:
-            with open(join(directory,submission_script), 'w+') as m:
+            with open(join(directory, submission_script), 'w+') as m:
                 for line in f:
                     if 'mem' in line:
-                        line = line.replace(line.split('=')[1], args.mem)
+                        line = line.replace(line.split('=')[1], args.mem + '\n')
                     if 'nodes' in line:
-                        line = line.replace(line.split('=')[1], args.nodes)
+                        line = line.replace(line.split('=')[1], args.nodes + '\n')
                     if 'time' in line:
-                        line = line.replace(line.split('=')[1], args.walltime)
+                        line = line.replace(line.split('=')[1], args.walltime + '\n')
                     if 'gres' in line:
-                        line = line.replace(line.split('=')[1], 'gpu:' + args.gpus)
+                        line = line.replace(line.split('=')[1], 'gpu:' + args.gpus + '\n')
                     if 'cpus' in line:
-                        line = line.replace(line.split('=')[1], args.cpus)
-
-                    m.write(line + '\n')
+                        line = line.replace(line.split('=')[1], args.cpus + '\n')
+                    m.write(line)
                
 def create_exec_script():
- 
     for directory in my_dict.itervalues():
         with open(exec_script_template) as f:
             with open(join(directory, exec_script), 'w+') as m:
@@ -98,10 +95,9 @@ def create_exec_script():
                     m.write(line)
 
 def create_header_script():
-
     for each_file, directory in my_dict.iteritems():
         with open(create_header_template) as f:
-            with open(join(directory,create_header),'w+') as m:
+            with open(join(directory, create_header),'w+') as m:
                     for line in f:
                         if "input_file =" in line:
                             line = line.replace("input_file =", "input_file = " + "'" + "../" + each_file + "'")
@@ -119,86 +115,120 @@ def calculate_atoms():
             atom_list.append(natoms)
 
     zipped_atomic_list = zip(files_to_run, atom_list)
-
     return zipped_atomic_list
 
+def read_vec_log():
+    vec_log = 'shared_log' + '/' + args.read_from + '.log'
+    if isfile(vec_log):
+        min_count = 0
+        sad_count = 0
+        disp_count = 0
+        natoms_read = 0
+        with open(vec_log) as f:
+            for row in f:
+                if 'min' in row:
+                    min_count = min_count + 1
+                if 'sad' in row:
+                    sad_count = sad_count + 1
+                if 'Displacement' in row:
+                    disp_count = disp_count + 1
+                if (re.findall(r'\s\d\s+[-]?\d[.]\d+\s+[-]?\d[.]\d+\s+[-]?\d[.]\d+\s+',row)):
+                    natoms_read = natoms_read + 1
 
-def create_gauss_art(zipped_atomic_list):
- 
+        natoms_read = natoms_read / (min_count + sad_count + disp_count)
+
+        min_sad_natoms_read = (min_count, sad_count, natoms_read)
+
+    if not isfile(vec_log):
+        min_sad_natoms_read = (0,0,0)
+
+    return min_sad_natoms_read
+
+def create_gauss_art(zipped_atomic_list, min_sad_natoms_read):
     for each_file, natoms in zipped_atomic_list:
         with open(gauss_art_template) as f:
             with open(join(each_file.split('.')[0], gauss_art), 'w+') as m:
                 for line in f:                     
                     if 'NATOMS' in line:
-                        line = line.replace(line.split('#')[0].split()[2], str(natoms))
+                        line = line.replace(line.split('#')[0].split()[2], str(natoms), 1)
                     if 'Max_Number_Events' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.max_events)
+                        line = line.replace(line.split('#')[0].split()[2], args.max_events, 1)
                     if 'Type_of_Events' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.type_events)
+                        line = line.replace(line.split('#')[0].split()[2], args.type_events, 1)
                     if 'Radius_Initial_Deformation' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.radius)
+                        line = line.replace(line.split('#')[0].split()[2], args.radius, 1)
                     if 'Central_Atom' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.centre_atm)
+                        line = line.replace(line.split('#')[0].split()[2], args.centre_atm, 1)
                     if 'Eigenvalue_Threshold' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.eigen_thresh)
+                        line = line.replace(line.split('#')[0].split()[2], args.eigen_thresh, 1)
                     if 'Exit_Force_Threshold' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.force_thresh)
+                        line = line.replace(line.split('#')[0].split()[2], args.force_thresh, 1)
                     if 'Increment_Size' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.inc_size)
+                        line = line.replace(line.split('#')[0].split()[2], args.inc_size, 1)
                     if 'Initial_Step_Size' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.init_step)
+                        line = line.replace(line.split('#')[0].split()[2], args.init_step, 1)
                     if 'sym_break_dist' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.sym_break)
+                        line = line.replace(line.split('#')[0].split()[2], args.sym_break, 1)
                     if 'Max_Perp_Moves_Basin' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.max_perp_basin)
+                        line = line.replace(line.split('#')[0].split()[2], args.max_perp_basin, 1)
                     if 'Min_Number_KSteps' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.min_k)
+                        line = line.replace(line.split('#')[0].split()[2], args.min_k, 1)
                     if 'Basin_Factor' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.basin_factor)
+                        line = line.replace(line.split('#')[0].split()[2], args.basin_factor, 1)
                     if 'Lanczos_SCLoop' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.lanczos)
+                        line = line.replace(line.split('#')[0].split()[2], args.lanczos, 1)
                     if 'Activation_MaxIter' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.activ_maxiter)
+                        line = line.replace(line.split('#')[0].split()[2], args.activ_maxiter, 1)
                     if 'delta_threshold' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.delta_thresh)
+                        line = line.replace(line.split('#')[0].split()[2], args.delta_thresh, 1)
                     if 'Max_Perp_Moves_Activ' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.max_perp_activ)
+                        line = line.replace(line.split('#')[0].split()[2], args.max_perp_activ, 1)
                     if 'Force_Threshold_Perp_Rel' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.force_thresh_perp)
+                        line = line.replace(line.split('#')[0].split()[2], args.force_thresh_perp, 1)
                     if 'Max_Iter_Basin' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.basin_maxiter)
+                        line = line.replace(line.split('#')[0].split()[2], args.basin_maxiter, 1)
                     if 'Write_xyz' in line:
-                        line = line.replace(line.split('#')[0].split()[2], '.' + args.write_xyz + '.')
+                        line = line.replace(line.split('#')[0].split()[2], '.' + args.write_xyz + '.', 1)
                     if 'Strategy_of_Search' in line:
-                        line = line.replace(line.split('#')[0].split()[2], args.search_strat)
+                        line = line.replace(line.split('#')[0].split()[2], args.search_strat, 1)
+                    if 'Shared_History_Filename' in line:
+                        line = line.replace(line.split('#')[0].split()[2], shared_log + each_file.split('.')[0] + '.log', 1)
+                    if 'Shared_History_To_Read' in line:
+                        line = line.replace(line.split('#')[0].split()[2], shared_log + args.read_from + '.log', 1)
+                    if 'nmin_read' in line:
+                        line = line.replace(line.split('#')[0].split()[2], str(min_sad_natoms_read[0]), 1)
+                    if 'nsad_read' in line:
+                        line = line.replace(line.split('#')[0].split()[2], str(min_sad_natoms_read[1]), 1)
+                    if 'natoms_read' in line:
+                        line = line.replace(line.split('#')[0].split()[2], str(min_sad_natoms_read[2]), 1)
 
                     m.write(line)
 
 def create_refconfig():
-
     for each_file, directory in my_dict.iteritems():
         with open(each_file) as f: 
             with open (join(directory, refconfig), 'w+') as m:
-                m.write(' run_id:         1000\n'  +  ' total_energy:   0\n')     
+                m.write(' run_id:         1000\n')     
+                m.write(' total_energy:   0 \n')     
+                m.write(' S   27.8481184348615        27.8481184348615        27.8481184348615 \n')     
                 for line in f:
-                    if (re.findall(r'\s\d\s+[-]?\d[.]\d+\s+[-]?\d[.]\d+\s+[-]?\d[.]\d+\s+',line)):
+                    if (re.findall(r'\s\d\s+[-]?\d[.]\d+\s+[-]?\d[.]\d+\s+[-]?\d[.]\d+\s+',line)): 
                         coords = re.findall(r'\s\d\s+[-]?\d[.]\d+\s+[-]?\d[.]\d+\s+[-]?\d[.]\d+\s+',line)[0]
                         m.write(coords)
 
 def create_filecounter():
-
     for directory in my_dict.itervalues():
-        with open(join(directory,filecounter),'w+') as f:
+        with open(join(directory, filecounter),'w+') as f:
             f.write("Counter:      1000")
 
 
 def submitting_scripts():
-
     for each_file, directory in my_dict.iteritems():
         print("Creating submission files for " + directory)
-
-        call(['sbatch', '-J', each_file.split('.')[0] + '_artgau', join(directory, submission_script)], shell = False) 
-
+        work_directory = getcwd()
+        chdir(join(work_directory, directory))
+        #call(['sbatch', '-J', each_file.split('.')[0], submission_script], shell = False) 
+        chdir(work_directory)
 
 
 if __name__ == '__main__':
@@ -208,7 +238,8 @@ if __name__ == '__main__':
     create_exec_script()
     create_header_script()
     zipped_atomic_list = calculate_atoms()
-    create_gauss_art(zipped_atomic_list)
+    min_sad_natoms_read = read_vec_log()
+    create_gauss_art(zipped_atomic_list, min_sad_natoms_read)
     create_refconfig()
     create_filecounter()
     submitting_scripts()
