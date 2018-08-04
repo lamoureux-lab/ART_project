98d875e227c1a36ff5a8fac932abecd3a4a01822
Bhupinder Singh <bhupindersingh.metneja@mail.concordia.ca>
2018-08-04 17:56:38 -0400

    Some changes

--- scripts/post_processing_scripts/check_cluster_members.py
+++ scripts/post_processing_scripts/check_cluster_members.py
@@ -4,27 +4,25 @@ from os.path import isfile
 
 
 parser = argparse.ArgumentParser(description = 'check member files of each cluster')
-parser.add_argument('-m','--min_files', nargs = '*', help = 'files_to_test')
-parser.add_argument('-tol','--dist_tol', default = 0.1, help = 'distance_tolerance')
+parser.add_argument('-f','--files', nargs = '*', help = 'files_to_test')
+parser.add_argument('-tol','--tol_define_clusters', default = 0.1, help = 'distance_tolerance')
 args = parser.parse_args()
 
-files_to_test = args.min_files
-tolerance = float(args.dist_tol)
+files_to_test = args.files
+tolerance = float(args.tol_define_clusters)
 
-mapping = cluster.calculate_cluster_map(files_to_test, tolerance)
-file_dict = cluster.order_cluster_mapping(mapping)
-
+cluster_map = cluster.mapping(files_to_test, tolerance)
 
 rep_name = ''    #name of representative file (representative of the cluster)
 member_name = '' #name of member file
-for key, value in file_dict.items():
+for key, value in cluster_map.items():
     rep_name = key + '_results.txt'
     for i in range(len(value)):
         if not value[i] == key:
             member_name = value[i] + '_results.txt' 
             with open(rep_name, 'r') as rep:
                 for line in rep:
-                    if ('Success!' in line) or ('Error!' in line):
+                    if ('verdict' in line) :
                         if not isfile(member_name):
                             with open(member_name, 'w+') as m:
                                 m.write(line + '\n' + 'Belongs to ' + key) 
